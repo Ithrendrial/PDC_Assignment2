86a5ae66f6afd43d0d7ee15713bdb18d0e8b4896
Ithrendrial <rcox299@aucklanduni.ac.nz>
2022-11-02 18:24:55 +1300

    GUI updates

--- Assignment2/src/assignment2/Main.java
+++ Assignment2/src/assignment2/Main.java
@@ -11,19 +11,20 @@ import javax.swing.JFrame;
  * @author rcox299
  */
 public class Main {
-        public static void main(String[] args) {
+
+    public static void main(String[] args) {
         GameView gameView = new GameView();
         GameModel gameModel = new GameModel();
-        
+
         JFrame window = new JFrame("The Plant Shop");
-        
+
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.getContentPane().add(gameView);
-        window.setSize(600,600);
+        window.setSize(600, 600);
         window.setVisible(true);
         window.setResizable(false);
         window.setLocationRelativeTo(null);
-        
+
         GameController controller = new GameController(gameView, gameModel, window);
         gameModel.addObserver(gameView);
     }
