c6f8be2656eb60b80eed20b8022820307594ee9e
Davis Pike <davisnpike@gmail.com>
2022-11-02 18:28:06 +1300

    Update GameDatabase.java

--- Assignment2/src/assignment2/GameDatabase.java
+++ Assignment2/src/assignment2/GameDatabase.java
@@ -3,7 +3,6 @@ package assignment2;
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
-import java.sql.SQLException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
