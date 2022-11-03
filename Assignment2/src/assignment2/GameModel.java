c4c2acbe1d08bb9bbb1e76b82790c4239abda528
Davis Pike <davisnpike@gmail.com>
2022-11-03 22:16:01 +1300

    Adding methods to Game Model

--- Assignment2/src/assignment2/GameModel.java
+++ Assignment2/src/assignment2/GameModel.java
@@ -11,6 +11,7 @@ public class GameModel extends Observable {
     public GameDatabase db;
     public GameData data;
     public String username;
+    Inventory userInventory;
 
     public GameModel() {
         this.db = new GameDatabase();
@@ -25,6 +26,7 @@ public class GameModel extends Observable {
         this.notifyObservers(this.data);
     }
 
+    //Methods for buying, selling, and propagating plants
     public void buyPlant(int plantID) {
         if (plantID == 1) {
             this.data.tier1One++;
@@ -126,7 +128,7 @@ public class GameModel extends Observable {
                 } else if (this.data.tier2One > 0) {
                     this.data.tier2One--;
                     this.data.tier1One = this.data.tier1One + 2;
-                } 
+                }
                 break;
             case 2:
                 if (this.data.tier5Two > 0) {
@@ -141,7 +143,7 @@ public class GameModel extends Observable {
                 } else if (this.data.tier2Two > 0) {
                     this.data.tier2Two--;
                     this.data.tier1Two = this.data.tier1Two = 2;
-                } 
+                }
                 break;
             case 3:
                 if (this.data.tier5Three > 0) {
@@ -156,7 +158,7 @@ public class GameModel extends Observable {
                 } else if (this.data.tier2Three > 0) {
                     this.data.tier2Three--;
                     this.data.tier1Three = this.data.tier1Three = 2;
-                } 
+                }
                 break;
             case 4:
                 if (this.data.tier5Four > 0) {
@@ -171,7 +173,7 @@ public class GameModel extends Observable {
                 } else if (this.data.tier2Four > 0) {
                     this.data.tier2Four--;
                     this.data.tier1Four = this.data.tier1Four + 2;
-                } 
+                }
                 break;
             case 5:
                 if (this.data.tier5Five > 0) {
@@ -186,7 +188,7 @@ public class GameModel extends Observable {
                 } else if (this.data.tier2Five > 0) {
                     this.data.tier2Five--;
                     this.data.tier1Five = this.data.tier1Five = 2;
-                } 
+                }
                 break;
             default:
                 break;
@@ -195,5 +197,31 @@ public class GameModel extends Observable {
 
     public void timePassed() {
         this.data.currentWeek++;
+    }
+
+    public void buyLight() {
+        this.data.growLight++;
+    }
+
+    public void buyFertiliser() {
+        this.data.fertiliser++;
+    }
+
+    public void useLight() {
+        if (this.data.growLight > 0) {
+            this.data.growLight--;
+            System.out.println("You used a grow light on your plant");
+        } else {
+            System.out.println("You do not own enough grow lights to do this.");
+        }
+    }
+    
+    public void useFertiliser() {
+        if (this.data.fertiliser > 0) {
+            this.data.fertiliser--;
+            System.out.println("You used fertiliser on your plant");
+        } else {
+            System.out.println("You do not own enough fertiliser to do this.");
+        }
     }
 }
